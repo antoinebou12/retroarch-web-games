c7631de321b118cf60a8a5c3e0e8538719038f95
Antoine Boucher <antoine.bou13@gmail.com>
2023-04-27 16:13:56 -0400

    Update InternetArchive.sh

--- InternetArchive.sh
+++ InternetArchive.sh
@@ -2,13 +2,12 @@
 
 url="https://archive.org/download/nointro.nes-headered"
 
-# Get the list of .7z files from the URL
-file_list=$(wget -q -O- "${url}" | grep -oE 'href="([^"#]+\.7z)"' | cut -d'"' -f2)
-
-# Download each .7z file
-for file in $file_list; do
-    echo "Downloading ${file}..."
-    wget -q --show-progress "${url}/${file}"
-done
+wget -qO- "$url" | \
+  grep -Eo '<td><a href="[^"]+\.7z' | \
+  head -1 | \
+  sed 's/^<td><a href="//' | \
+  while read -r file; do
+    wget -nc --show-progress --quiet "$url/$file"
+  done
 
 echo "All .7z files have been downloaded."
