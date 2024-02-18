09fe3396190c3670bcaade1a1ef555fcce1512ea
Antoine Boucher <antoine.bou13@gmail.com>
2024-02-17 22:27:37 -0500

    Update InternetArchive.sh

--- InternetArchive.sh
+++ InternetArchive.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+
 download_7z_files() {
   url="$1"
   wget -qO- "$url" | \
@@ -23,3 +26,26 @@ urls=(
 )
 
 parallel -j 6 download_7z_files ::: "${urls[@]}"
+
+
+download_zip_files() {
+  url="$1"
+  wget -qO- "$url" | \
+    grep -Eo '<td><a href="[^"]+\.zip' | \
+    head -1 | \
+    sed 's/^<td><a href="//' | \
+    while read -r file; do
+      wget -nc --show-progress --quiet "$url/$file"
+    done
+}
+
+export -f download_zip_files
+
+urls=(
+  "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy/"
+  "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20%28Private%29/"
+  "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20Advance/"
+  "https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Game%20Boy%20Color/"
+)
+
+parallel -j 4 download_zip_files ::: "${urls[@]}"
